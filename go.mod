970a9440c620c48d53e7228f0d3f009346ba99c9
Michael Frayer <frayerm@gmail.com>
2020-07-09 19:04:25 -0400

    Initial commit with base types

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/frayer/concourse-resource-tekton-trigger
+
+go 1.14
---
+++ internal/concourse/types.go
@@ -0,0 +1,42 @@
+package concourse
+
+type PipelineState struct {
+	Name      string     `json:"name"`
+	Resources []Resource `json:"resources"`
+	Jobs      []Job      `json:"jobs"`
+}
+
+type Job struct {
+	Name    string         `json:"name"`
+	Inputs  []ResourceStep `json:"inputs,omitempty"`
+	Outputs []ResourceStep `json:"outputs,omitempty"`
+	Builds  []Build        `json:"builds,omitempty"`
+}
+
+type ResourceStep struct {
+	Name    string   `json:"resource"`
+	Passed  []string `json:"passed,omitempty"`
+	Trigger bool     `json:"trigger,omitempty"`
+}
+
+type Build struct {
+	Id           string          `json:"id"`
+	BuildInputs  []BuildResource `json:"buildInputs"`
+	BuildOutputs []BuildResource `json:"buildOutputs"`
+	Status       string          `json:"string"`
+}
+
+type BuildResource struct {
+	ResourceName  string `json:"resourceName"`
+	VersionSha256 string `json:"versionSha256"`
+}
+
+type Resource struct {
+	Name               string    `json:"name"`
+	DiscoveredVersions []Version `json:"discoveredVersions"`
+}
+
+type Version struct {
+	Sha256        string `json:"sha256"`
+	DiscoveryDate string `json:"discoveryDate"`
+}
